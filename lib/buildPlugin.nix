d4dfad844caa114af035ce1452afd90ef0ed7331
Jordan Isaacs <github@jdisaacs.com>
2021-09-14 22:02:30 -0400

    install grammers the nix way

--- lib/buildPlugin.nix
+++ lib/buildPlugin.nix
@@ -2,11 +2,17 @@
 final: prev:
 let
   inherit (prev.vimUtils) buildVimPluginFrom2Nix;
+
+  treesitterGrammars = prev.tree-sitter.withPlugins (_: prev.tree-sitter.allGrammars);
   
   buildPlug = name: buildVimPluginFrom2Nix {
     pname = name;
     version = "master";
     src = builtins.getAttr name inputs;
+    postPatch = if (name == "nvim-treesitter") then ''
+      rm -r parser
+      ln -s ${treesitterGrammars} parser
+    '' else "";
   };
 in
 {
