c4b260baa25d42a3a715cd5396a534818edfa12b
Mona Mayrhofer <mona.mayrhofer@proton.me>
2023-05-02 11:27:17 +0200

    Keymap: Add Actions for Trouble.nvim

--- modules/lsp/trouble.nix
+++ modules/lsp/trouble.nix
@@ -15,16 +15,18 @@ in {
   };
 
   config = mkIf (cfg.enable && cfg.trouble.enable) {
-    vim.startPlugins = ["trouble"];
-
-    vim.nnoremap = {
-      "<leader>xx" = "<cmd>TroubleToggle<CR>";
-      "<leader>lwd" = "<cmd>TroubleToggle workspace_diagnostics<CR>";
-      "<leader>ld" = "<cmd>TroubleToggle document_diagnostics<CR>";
-      "<leader>lr" = "<cmd>TroubleToggle lsp_references<CR>";
-      "<leader>xq" = "<cmd>TroubleToggle quickfix<CR>";
-      "<leader>xl" = "<cmd>TroubleToggle loclist<CR>";
+    nvim-flake.keymapActions = with nvim.keymap; {
+      trouble = {
+        toggle = mkVimAction "<cmd>TroubleToggle<CR>";
+        workspaceDiagnostics = mkVimAction "<cmd>TroubleToggle workspace_diagnostics<CR>";
+        documentDiagnostics = mkVimAction "<cmd>TroubleToggle document_diagnostics<CR>";
+        lspReferences = mkVimAction "<cmd>TroubleToggle lsp_references<CR>";
+        quickfix = mkVimAction "<cmd>TroubleToggle quickfix<CR>";
+        loclist = mkVimAction "<cmd>TroubleToggle loclist<CR>";
+      };
     };
+
+    vim.startPlugins = ["trouble"];
 
     vim.luaConfigRC.trouble = nvim.dag.entryAnywhere ''
       -- Enable trouble diagnostics viewer
