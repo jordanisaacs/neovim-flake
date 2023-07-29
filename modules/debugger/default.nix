f92e8c0c02eb6612f465d5f4d3e83a79d3877010
Jordan Isaacs <mail@jdisaacs.com>
2023-07-28 20:21:01 -0700

    more

--- modules/debugger/default.nix
+++ modules/debugger/default.nix
@@ -48,12 +48,12 @@ in {
         dap.adapters.lldb = codelldb;
         dap.adapters.rt_lldb = codelldb;
 
-        vim.keymap.set("n", "<leader>do", require'dap'.repl.open")
+        vim.keymap.set("n", "<leader>do", require'dap'.repl.open)
 
-        vim.keymap.set("n", "<leader>dc", require'dap'.continue")
-        vim.keymap.set("n", "<leader>dsn", require'dap'.step_over")
-        vim.keymap.set("n", "<leader>dsi", require'dap'.setp_into")
-        vim.keymap.set("n", "<leader>dso", require'dap'.step_out")
+        vim.keymap.set("n", "<leader>dc", require'dap'.continue)
+        vim.keymap.set("n", "<leader>dsn", require'dap'.step_over)
+        vim.keymap.set("n", "<leader>dsi", require'dap'.step_into)
+        vim.keymap.set("n", "<leader>dso", require'dap'.step_out)
 
         vim.keymap.set("n", "<leader>db", require'dap'.toggle_breakpoint)
         vim.keymap.set("n", "<leader>dB", function() require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
